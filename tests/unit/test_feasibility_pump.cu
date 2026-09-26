c0a627cdc6e49eb12954993cb88af342c3bee8b3
adityapach26 <adityapach26@gmail.com>
2026-09-26 14:43:54 +0530

    Fix Phase 23.1 feasibility pump test

--- tests/unit/test_feasibility_pump.cu
+++ tests/unit/test_feasibility_pump.cu
@@ -1,5 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <catch2/catch_approx.hpp>
 #include <vector>
 
 #include "cuda/feasibility_pump.cuh"
